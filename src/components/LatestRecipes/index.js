946d45cc62c81d401508ccc153b7923344e5dbda
Aman Kumar <9k725304@gmail.com>
2024-06-03 16:45:16 +0530

    implement filter by first name logic that filter recipe by it's first char

--- src/components/LatestRecipes/index.js
+++ src/components/LatestRecipes/index.js
@@ -1,13 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 
 import RecipeCard from "../RecipeCard";
 import Loader from "../Loader";
 
 import "./index.css"
+import FilterButtons from "../FilterButtons";
+
+// button list
+const buttonList = [];
+for(let i=65; i<91; i++){
+    buttonList.push({id: uuidv4(), value: String.fromCharCode(i)})
+}
+
+console.log(buttonList)
 
 const LatestRecipes = () => {
 
     const [recipesList, setRecipesList] = useState([]);
+    const [activeChar, setActiveChar] = useState('');
+
+    const onFilterByFirstChar = char => setActiveChar(char);
 
     const getData = async() => {
         const url = 'https://chinese-food-db.p.rapidapi.com/';
@@ -35,7 +49,22 @@ const LatestRecipes = () => {
 
     // render recipes cards
     const renderRecipeCardView = () =>{
-        return recipesList.map(recipe => <RecipeCard key={recipe.id} recipeDetails={recipe} />)
+        const filteredRecipeList = activeChar !== "" ? recipesList.filter(each => each.title.charAt(0).toUpperCase() === activeChar) : recipesList;
+        return (
+            <>
+                {
+                    filteredRecipeList.map(recipe => <RecipeCard key={recipe.id} recipeDetails={recipe} />)
+                }
+                <div className="col-12 mt-5 mb-5">                   
+                    <h2 className="latest-recipe mt-5 mb-4">Brows By Name</h2>  
+                    <div className="mt-5 d-flex justify-content-center align-item-center flex-wrap">
+                        {
+                            buttonList.map(each => <FilterButtons key={each.id} buttonDetails={each} onFilterByFirstChar={onFilterByFirstChar} />)
+                        } 
+                    </div> 
+                </div>
+            </>
+        );
     }
 
     return (
