9e58c6ae7685c5ffacedb7965e8be4aee2fa8fbd
Aman Kumar <9k725304@gmail.com>
2024-06-03 16:46:16 +0530

    remove border around loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,7 +3,7 @@ import { Circles } from 'react-loader-spinner';
 const Loader = () => {
     return (
         <div className='col-12 mt-5'>
-            <div className="d-flex justify-content-center border p-5">
+            <div className="d-flex justify-content-center p-5">
                 <Circles height="30" width="30" color="blue" ariaLabel="loading"  />
             </div>
         </div>
