5bb1a4817090373709eee5dabd1561eb6722bda3
Aman Kumar <9k725304@gmail.com>
2024-06-07 15:48:10 +0530

    restructure App component with and implementing routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import {Component} from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './components/Home'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutSection from './components/AboutSection';
 
 import './App.css';
 
-const App = () => {
+class App extends Component{
 
-    return <Home />
+    render(){
+        return <div className='app-bg'>
+            <BrowserRouter>
+                <Header />
+                    <Routes>
+                        <Route exact path='/' Component={Home} />
+                        <Route exact path='/about' Component={AboutSection} />
+                    </Routes>  
+                <Footer />                  
+            </BrowserRouter>
+        </div>
+    }
 }
 
 export default App;
