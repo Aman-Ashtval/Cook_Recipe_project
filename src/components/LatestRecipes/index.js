423194b32aeeba300b31b0407840ebb958ec547b
Aman Kumar <9k725304@gmail.com>
2024-06-09 17:57:01 +0530

    restructure views

--- src/components/LatestRecipes/index.js
+++ src/components/LatestRecipes/index.js
@@ -4,14 +4,23 @@ import { v4 as uuidv4 } from 'uuid';
 
 import RecipeCard from "../RecipeCard";
 import Loader from "../Loader";
+import NoItemView from "../NoItemView";
+import FailureView from "../FailureView";
+import FilterButtons from "../FilterButtons";
 
 import "./index.css"
-import FilterButtons from "../FilterButtons";
 
 // button list
 const buttonList = [];
 for(let i=65; i<91; i++){
     buttonList.push({id: uuidv4(), value: String.fromCharCode(i)})
+}
+
+// response constants
+const responseConstant = {
+    success: "SUCCESS",
+    failure: "FAILURE",
+    inProgress: "IN_PROGRESS",
 }
 
 
@@ -21,6 +30,7 @@ const LatestRecipes = props => {
 
     const [recipesList, setRecipesList] = useState([]);
     const [activeChar, setActiveChar] = useState('');
+    const [responseStatus, setResponseStatus] = useState(responseConstant.inProgress)
 
     const onFilterByFirstChar = char => setActiveChar(char);
 
@@ -33,12 +43,15 @@ const LatestRecipes = props => {
                 'x-rapidapi-host': 'chinese-food-db.p.rapidapi.com'
             }
         };
-        try {
-            const response = await fetch(url, options);
+
+        const response = await fetch(url, options);
+
+        if(response.ok){
             const data = await response.json();
             setRecipesList(data);
-        } catch (error) {
-            console.error(error);
+            setResponseStatus(responseConstant.success);
+        }else{
+            setResponseStatus(responseConstant.failure);
         }
     }
 
@@ -48,29 +61,46 @@ const LatestRecipes = props => {
 
 
     // render recipes cards
-    const renderRecipeCardView = () =>{
+    const renderSuccessView = () => {
         const filteredRecipeList = activeChar !== "" ? recipesList.filter(each => each.title.charAt(0).toUpperCase() === activeChar) : recipesList;
         const filterRecipeByUserInput = filteredRecipeList.filter(each => each.title.toLowerCase().includes(userInput.toLowerCase()));
         return (
             <>
                 {
-                    filterRecipeByUserInput.map(recipe => <RecipeCard key={recipe.id} recipeDetails={recipe} />)
+                    filterRecipeByUserInput.length === 0 ? <NoItemView /> : filterRecipeByUserInput.map(recipe => <RecipeCard key={recipe.id} recipeDetails={recipe} />)
                 }
-                <div className="col-12 mt-5 mb-5">                   
-                    <h2 className="latest-recipe mt-5 mb-4">Brows By Name</h2>  
-                    <div className="mt-5 d-flex justify-content-center align-item-center flex-wrap">
-                        {
-                            buttonList.map(each => <FilterButtons key={each.id} buttonDetails={each} onFilterByFirstChar={onFilterByFirstChar} />)
-                        } 
-                    </div> 
-                </div>
+                {
+                    (filterRecipeByUserInput.length !== 0) && 
+                    <div className="col-12 mt-5 mb-5">                   
+                        <h2 className="latest-recipe mt-5 mb-4">Brows By Name</h2>  
+                        <div className="mt-5 d-flex justify-content-center align-item-center flex-wrap">
+                            {
+                                buttonList.map(each => <FilterButtons key={each.id} buttonDetails={each} onFilterByFirstChar={onFilterByFirstChar} />)
+                            } 
+                        </div> 
+                    </div>
+                }
             </>
         );
     }
 
+    const renderRecipeCardView = () => {
+        switch (responseStatus) {
+            case responseConstant.success: 
+                return renderSuccessView();
+            case responseConstant.failure: 
+                return <FailureView />;
+            case responseConstant.inProgress: 
+                return <Loader />;                                
+        
+            default:
+                return null;
+        }
+    }
+
     return (
         <>
-            {recipesList.length === 0 ? <Loader /> : renderRecipeCardView()}
+            {renderRecipeCardView()}
         </>
     )
 }
