7968d0044859d27e1e699cb7b9b85e1952808c8a
Aman Kumar <9k725304@gmail.com>
2024-06-09 17:58:03 +0530

    adding new Routes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import Footer from './components/Footer';
 import AboutSection from './components/AboutSection';
 import ContactSection from './components/ContactSection';
 import RecipeDetails from './components/RecipeDetails';
+import FavoriteItem from './components/FavoriteItem';
 
 import './App.css';
 
 class App extends Component{
+
+    componentDidMount(){
+        localStorage.setItem('favoriteRecipesList', JSON.stringify([]));
+    }
 
     render(){
         return <div className='app-bg'>
@@ -21,6 +26,7 @@ class App extends Component{
                         <Route exact path='/about' Component={AboutSection} />
                         <Route exact path='/contact' Component={ContactSection} />
                         <Route exact path='/recipe/:id' Component={RecipeDetails} />
+                        <Route exact path='/favorite' Component={FavoriteItem} />
                     </Routes>  
                 <Footer />                  
             </BrowserRouter>
